8864bf2d57428614a30c03c04ca2138747ca51e2
MA Asyraf <maasyraf.edu@gmail.com>
2023-12-09 01:02:22 +0700

    update on iterating process

--- scene_generation/generate_dataset.py
+++ scene_generation/generate_dataset.py
@@ -26,10 +26,20 @@ def load_speech_signal(speech_file_path: str) -> tuple[np.ndarray, np.ndarray]:
     # load wav signal
     speech_rate, speech_signal = wavfile.read(speech_filename)
 
-    print(speech_signal.dtype)
+    # checking datatype
+    i = np.iinfo(np.dtype(speech_signal.dtype))
+    abs_max = pow(2, (i.bits - 1))
+    offset = i.min + abs_max
 
+    speech_signal = (speech_signal.astype(speech_signal.dtype) - offset) / abs_max
 
     return speech_rate, speech_signal
+
+def write_speech_signal(output_filename: str, speech_rate: int, speech_data: np.ndarray) -> None:
+    """
+    The function to write speech data into wav file
+    """
+    wavfile.write(output_filename, speech_rate, speech_data.astype(np.float64))
 
 def load_impulse_response(ir_file_path: str) -> tuple[np.ndarray, np.ndarray]:
     """
@@ -46,11 +56,11 @@ def load_impulse_response(ir_file_path: str) -> tuple[np.ndarray, np.ndarray]:
     # get values for impulser response for left and right channels
     ir_data = ir.Data.IR.get_values()
     ir_left = ir_data[:, 0, :]
-    ir_right = ir_data[:, 0, :]
+    ir_right = ir_data[:, 1, :]
 
     return ir_left, ir_right
 
-def filter_ir(signal: ndarray, hrtf_left: ndarray, hertf_right: ndarray) -> np.ndarray:
+def filter_ir(signal: ndarray, ir_left: ndarray, ir_right: ndarray) -> np.ndarray:
     """
     The function for applying convolutive filter to get filtered (reverberant) speech signals
     """
@@ -80,9 +90,7 @@ def check_previous_created_data(output_dir: str | Path) -> list[str]:
     if not output_dir.exists():
         return []
 
-    previous_created_data = []
-    for i in output_dir.glob("train/"):
-        previous_created_data.append(i.name)
+    previous_created_data = [x for x in output_dir.glob('**/*') if x.is_file()]
 
     return previous_created_data
 
@@ -100,14 +108,14 @@ def create_speech_data(cfg: DictConfig) -> None:
         logger.warning(f"Found {len(previous_data)} created samples \n")
 
     # create speech data with defined scenes
-
-    # steps:
-    # 1 - iteration for reverberant types
     ir_dir = Path(cfg.path.impulse_response_dir).glob('**/*')
     ir_files = [x for x in ir_dir if x.is_file()]
 
     logger.info(f"Generating acoustic scene with impulse response (IR) on: {cfg.path.impulse_response_dir}")
     logger.info(f"There are {len(ir_files)} IR files found")
+
+    # check previous existed data
+    previous_samples = check_previous_created_data(cfg.path.output_dir)
 
     for ir_path in ir_files:
         # define ir scenes name
@@ -153,25 +161,28 @@ def create_speech_data(cfg: DictConfig) -> None:
                 speech_path = Path(cfg.path.speech_dir) / speaker
                 speech_list = [x for x in speech_path.glob('**/*') if x.is_file()]
 
-                for speech_path in speech_list:
+                for idx, speech_path in enumerate(speech_list, 1):
+
+                    # checking output data
+                    speech_name = str.split(str(speech_path), sep='/')[-1]
+                    speech_number = speech_name[-8:-4]
+                    output_path = scene_path / speech_name
+
+                    if output_path in previous_samples:
+                        logger.info(f"Skipping speech number {speech_number} [{idx}/{len(speech_list)}]")
+                        continue
+
+                    logger.info(f"Processing speech number {speech_number} [{idx}/{len(speech_list)}]")
+
                     # load signal data
                     rate, speech = load_speech_signal(speech_path)
-                    print(speech_path)
 
-                    a = b
+                    # process speech data to create reverberant signal
+                    speech_rev_left, speech_rev_right = filter_ir(speech, ir_left[degree,:], ir_right[degree,:])
+                    speech_rev = np.array([speech_rev_left, speech_rev_right]).T
 
-
-        # scene_path = Path(cfg.path.output_dir) / ir_scenes / ir_degree / speaker / sample_name
-        # scene_path.mkdir(parents=True, exist_ok=True)
-
-        # apply filtering to signal
-        # for degree in ir_left:
-
-
-
-
-    # 2 - iteration for speech signals processing
-
+                    # write wav data
+                    write_speech_signal(output_path, rate, speech_rev)
 
 if __name__ == "__main__":
     create_speech_data()
